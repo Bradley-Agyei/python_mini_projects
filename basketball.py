c4ecc69501bc0bf98d80715dcd4e52218d8ec5fa
Bradley Agyei <Bradley-Agyei@users.noreply.github.com>
2022-01-27 23:13:53 -0500

    modified to obtain players full name and current team

--- basketball.py
+++ basketball.py
@@ -1,5 +1,7 @@
 import requests
 
+#No API key needed 
+
 #URL that request is being made to 
 BASE_URL = "https://www.balldontlie.io/api/v1/players"
 
@@ -15,7 +17,11 @@ response = requests.get(request_url)
 #Check status code of request
 if response.status_code == 200:
     data = response.json()
-    print(data)
+    #Grab full name of player 
+    full_name = data["first_name"] + " " + data["last_name"]
+    #Grab team the player plays for
+    team = data["team"]["name"]
+    print(f"{full_name} plays for {team}.")
 else:
     print("Error retrieving data.")
 
