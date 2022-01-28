6e9c44196c0634b39cc58e5b0805461c548a4046
Bradley Agyei <Bradley-Agyei@users.noreply.github.com>
2022-01-27 23:03:15 -0500

    Updated basketball project to obtain specific player data

--- basketball.py
+++ basketball.py
@@ -1,18 +1,20 @@
-from urllib import response
 import requests
 
 #URL that request is being made to 
 BASE_URL = "https://www.balldontlie.io/api/v1/players"
 
-#request_url
-request_url = f"{BASE_URL}"
+#requesting player id 
+player_id = input("Enter a player id: ")
+
+#request_url to get specific player based on ID
+request_url = f"{BASE_URL}/{player_id}"
 
 #GET request to retrieve data
-response1 = requests.get(request_url) 
+response = requests.get(request_url) 
 
 #Check status code of request
-if response1.status_code == 200:
-    data = response1.json()
+if response.status_code == 200:
+    data = response.json()
     print(data)
 else:
     print("Error retrieving data.")
